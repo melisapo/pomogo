6e9085fe2c08923321daba51f68ed0d121b46a01
melissa <melissagh3009@gmail.com>
2026-02-14 14:44:20 -0500

    add model struct

--- main.go
+++ main.go
@@ -1,8 +1,46 @@
 package main
 
 import (
-	
-	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
+	"time"
+
 	"github.com/charmbracelet/bubbles/textinput"
 )
+
+type state int
+
+const (
+	stateSetup state = iota
+	stateRunning
+)
+
+type sessionType int
+
+const (
+	sessionFocus sessionType = iota
+	sessionBreak
+)
+
+type tickMsg time.Time
+
+type model struct {
+	state       state
+	sessionType sessionType
+
+	// Setup
+	focusInput   textinput.Model
+	breakInput   textinput.Model
+	currentInput int // 0 = focus, 1 = break
+
+	// Timer
+	focusDuration     time.Duration
+	breakDuration     time.Duration
+	timeLeft          time.Duration
+	running           bool
+	completedSessions int
+
+	width  int
+	height int
+}
+
+
+
